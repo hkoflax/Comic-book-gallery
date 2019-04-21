1de0eb8030f56200a09ec5b6d5ad1eaf7313954d
hkoflax <hkoflax@users.noreply.github.com>
2019-04-21 18:41:29 -0400

    Add Comic Book controller

---
+++ src/ComicBookgallery/ComicBookgallery/Controllers/ComicBookController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookgallery.Controllers
+{
+    public class ComicBookController : Controller
+    {
+        // GET: ComicBook
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
