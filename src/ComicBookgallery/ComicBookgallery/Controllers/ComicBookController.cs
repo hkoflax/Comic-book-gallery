498a7984f85c7bec892c9c61da760722f0e8ad9d
hkoflax <hkoflax@users.noreply.github.com>
2019-04-21 22:50:03 -0400

    Update comic book to use repository

--- src/ComicBookgallery/ComicBookgallery/Controllers/ComicBookController.cs
+++ src/ComicBookgallery/ComicBookgallery/Controllers/ComicBookController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookgallery.Models;
+﻿using ComicBookgallery.Data;
+using ComicBookgallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,23 +11,18 @@ namespace ComicBookgallery.Controllers
     public class ComicBookController : Controller
     {
         // GET: ComicBook
-        public ActionResult Detail()
+        private ComicBookRepository _comicBookRepository = null;
+        public ComicBookController()
         {
-            var comicBook = new ComicBook()
+            _comicBookRepository = new ComicBookRepository();
+        }
+        public ActionResult Detail(int? id)
+        {
+            if (id==null)
             {
-                SeriesTitle = "The Amazing spider man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() {Name="dan slott", Role="script"},
-                    new Artist() {Name="humberto ramos", Role="pencils"},
-                    new Artist() {Name="victor olazaba", Role="inks"},
-                    new Artist() {Name="edgar delgado", Role="colors"},
-                    new Artist() {Name="chris eliopoulos", Role="letters"},
-                }
-            };
-
+                return HttpNotFound();
+            }
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
             return View(comicBook);
         }
     }
--- src/ComicBookgallery/ComicBookgallery/Models/ComicBook.cs
+++ src/ComicBookgallery/ComicBookgallery/Models/ComicBook.cs
@@ -7,7 +7,7 @@ namespace ComicBookgallery.Models
 {
     public class ComicBook
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
