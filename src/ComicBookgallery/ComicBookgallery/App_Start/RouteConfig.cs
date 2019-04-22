cc08d4db1dd995604181134629209b285ef11774
hkoflax <hkoflax@users.noreply.github.com>
2019-04-21 23:24:54 -0400

    updated the default route

--- src/ComicBookgallery/ComicBookgallery/App_Start/RouteConfig.cs
+++ src/ComicBookgallery/ComicBookgallery/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ComicBookgallery
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ComicBook", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
