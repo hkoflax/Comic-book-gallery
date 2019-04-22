5cb4bd8ce28ab11e2c45143da7830b0a830be860
hkoflax <hkoflax@users.noreply.github.com>
2019-04-21 20:01:09 -0400

    Update detail view

--- src/ComicBookgallery/ComicBookgallery/Controllers/ComicBookController.cs
+++ src/ComicBookgallery/ComicBookgallery/Controllers/ComicBookController.cs
@@ -11,6 +11,17 @@ namespace ComicBookgallery.Controllers
         // GET: ComicBook
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "The Amazing spider man";
+            ViewBag.Issuenumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "script: dan slott",
+                "pencils: humberto ramos",
+                "inks: victor olazaba",
+                "colors: edgar delgado",
+                "letters: chris eliopoulos"
+            };
             return View();
         }
     }
