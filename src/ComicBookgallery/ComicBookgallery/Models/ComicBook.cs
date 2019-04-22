ee37eeb6f19478530d667d9b18600b000b36189d
hkoflax <hkoflax@users.noreply.github.com>
2019-04-21 20:41:13 -0400

    updated comic book model

--- src/ComicBookgallery/ComicBookgallery/Models/ComicBook.cs
+++ src/ComicBookgallery/ComicBookgallery/Models/ComicBook.cs
@@ -9,9 +9,25 @@ namespace ComicBookgallery.Models
     {
         public int id { get; set; }
         public string SeriesTitle { get; set; }
-        public int issueNumber { get; set; }
+        public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool favorite { get; set; }
+
+        public string Displaytext
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+        //series-title-issueNumber.jpg
+        public string CoverImagefileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
