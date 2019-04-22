ae8d0658de132575de202edd221517b6800603f4
hkoflax <hkoflax@users.noreply.github.com>
2019-04-21 20:35:35 -0400

    added comic book and artist model

---
+++ src/ComicBookgallery/ComicBookgallery/Models/ComicBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookgallery.Models
+{
+    public class ComicBook
+    {
+        public int id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int issueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool favorite { get; set; }
+    }
+}
